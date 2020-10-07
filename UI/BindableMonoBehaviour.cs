8b3630c1c231b15569fd849eb0e0db2ec59df94d
Paul Redetzky <p_redetzky@gmx.de>
2020-10-07 22:14:30 +0200

    Update BindableMonoBehaviour.cs

--- UI/BindableMonoBehaviour.cs
+++ UI/BindableMonoBehaviour.cs
@@ -11,8 +11,10 @@ using System.ComponentModel;
 /// <typeparam name="T">The model class which should be bindable.</typeparam>
 public abstract class BoundMonoBehaviour<T> : MonoBehaviour where T : class, INotifyPropertyChanged
 {
+    public T data { get; private set; }
     public void Bind(T data)
     {
+        this.data = data;
         data.PropertyChanged += Data_PropertyChanged;
     }
 
